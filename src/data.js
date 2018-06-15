d678babed43c98afd29b32a29df37c415f2ef0a2
marimeli <marimel.casas@gmail.com>
2018-06-15 15:15:22 -0500

    añadiendo imágenes al readme

--- src/data.js
+++ src/data.js
@@ -1,18 +1,17 @@
 const getJSON = (url, callback) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = _ => {
-      if (xhr.readyState === 4) {
-        if (xhr.status !== 200) {
-          return callback(new Error(`HTTP error: ${xhr.status}`));
-        }
-        try {
-          callback(null, JSON.parse(xhr.responseText));
-        } catch (err) {
-          callback(err);
-        }
+  const xhr = new XMLHttpRequest();
+  xhr.onload = _ => {
+    if (xhr.readyState === 4) {
+      if (xhr.status !== 200) {
+        return callback(new Error(`HTTP error: ${xhr.status}`));
       }
-    };
-  
-    xhr.open('GET', url);
-    xhr.send();  
+      try {
+        callback(null, JSON.parse(xhr.responseText));
+      } catch (err) {
+        callback(err);
+      }
+    }
   };
+  xhr.open('GET', url);
+  xhr.send();
+};
