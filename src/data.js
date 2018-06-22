a8c0b6f01316a1fbdc941b6613c1a421d92ee359
natachang <nataliechangch99@gmail.com>
2018-06-22 14:18:52 -0500

    cambio

--- src/data.js
+++ src/data.js
@@ -37,50 +37,5 @@ const getJSON = (url, callback) => {
     xhr.send();
 };
 
-<<<<<<< HEAD
 
 
-=======
-const processCohortData = (options) => {
-    (cohort) => {
-        let options = {
-            cohort: {},
-            cohortData: {
-                user: [],
-                progress: {}
-            },
-            orderBy: '',
-            orderDirection: '',
-            search: ''
-        };
-
-    }
-},
-
-
-const computeUsersStats = (users, progress, courses) => {
-
-    let usersWithStats = (user) => {
-        let stats = {
-            percent: 0,
-            exercises: {
-                total: 0,
-                completed: 0,
-                percent: 0
-            },
-            reads: {
-                total: 0,
-                completed: 0,
-                percent: 0
-            },
-            quizzes: {
-                total: 0,
-                completed: 0,
-                percent: 0,
-                scoreSum: 0,
-                scoreAvg: 0
-            }
-        },
-    }
-};
->>>>>>> b188a7024efb0b9f2d10fefd00d146be23339130
--- src/main.js
+++ src/main.js
@@ -7,17 +7,14 @@ getJSON('../data/cohorts.json', (err, json) => {
     if (err) {
         return console.error(err);
     }
-<<<<<<< HEAD
 
 
-=======
->>>>>>> b188a7024efb0b9f2d10fefd00d146be23339130
     const cohorts = json;
 
 
     for (let i = 0; i < cohorts.length; i++) {
         const optionsElements = document.createElement('option');
-        const contenidoOptions = document.createTextNode('hola');
+        const contenidoOptions = document.createTextNode(cohorts[i].id);
         optionsElements.appendChild(contenidoOptions);
         selectElement.appendChild(optionsElements);
         console.log(cohorts[i].id);
@@ -25,28 +22,3 @@ getJSON('../data/cohorts.json', (err, json) => {
 });
 
 
-<<<<<<< HEAD
-=======
-getJSON('../data/cohorts/lim-2018-03-pre-core-pw/users.json', (err, json) => {
-    if (err) {
-        return console.error(err);
-    }
-    const users = json;
-    for (let i = 0; i < users.length; i++) {
-        optionsElements = document.createElement('option');
-        contenidoOptions = document.createTextNode(users[i].name);
-        optionsElements.appendChild(contenidoOptions);
-        selectUsers.appendChild(optionsElements);
-        console.log(users[i].name); 
-        }
-
-    getJSON('../data/cohorts/lim-2018-03-pre-core-pw/progress.json', (err, json) => {
-        if (err) {
-            return console.error(err);
-        }
-        const usersProgress = json;
-
-        console.log(users, usersProgress)
-    });
-});
->>>>>>> b188a7024efb0b9f2d10fefd00d146be23339130
