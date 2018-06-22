1749c2e634b3c33a39d4bfcf5223e6f2b276d3a3
natachang <nataliechangch99@gmail.com>
2018-06-22 16:10:22 -0500

    actualizando

--- src/data.js
+++ src/data.js
@@ -1,23 +1,3 @@
-
-window.processCohortData = (options) => {
-
-};
-
-
-
-window.computeUsersStats = (users, progress, courses) => {
-   
-};
-
-window.sortUsers = (users, orderBy, orderDirection) => {
-
-};
-
-window.filterUsers = (users, search) => {
-
-};
-  
-
 const getJSON = (url, callback) => {
     const xhr = new XMLHttpRequest();
     xhr.onload = _ => {
@@ -37,5 +17,47 @@ const getJSON = (url, callback) => {
     xhr.send();
 };
 
+/* const processCohortData = (options) => {
+    (cohort) => {
+        let options = {
+            cohort: {},
+            cohortData: {
+                user: [],
+                progress: {}
+            },
+            orderBy: '',
+            orderDirection: '',
+            search: ''
+        };
+
+    }
+},
+
+
+const computeUsersStats = (users, progress, courses) => {
+
+    let usersWithStats = (user) => {
+        let stats = {
+            percent: 0,
+            exercises: {
+                total: 0,
+                completed: 0,
+                percent: 0
+            },
+            reads: {
+                total: 0,
+                completed: 0,
+                percent: 0
+            },
+            quizzes: {
+                total: 0,
+                completed: 0,
+                percent: 0,
+                scoreSum: 0,
+                scoreAvg: 0
+            }
+        },
+    }
+}; */
 
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-
 const selectElement = document.getElementById('cohorts');
 const selectUsers= document.getElementById('users');
 const buttonGlobal = document.getElementById('global');
@@ -14,10 +12,7 @@ getJSON('../data/cohorts.json', (err, json) => {
     if (err) {
         return console.error(err);
     }
-
-
     const cohorts = json;
-
 
     for (let i = 0; i < cohorts.length; i++) {
         const optionsElements = document.createElement('option');
@@ -28,4 +23,27 @@ getJSON('../data/cohorts.json', (err, json) => {
     }
 });
 
+
+getJSON('../data/cohorts/lim-2018-03-pre-core-pw/users.json', (err, json) => {
+    if (err) {
+        return console.error(err);
+    }
+    const users = json;
+    for (let i = 0; i < users.length; i++) {
+        optionsElements = document.createElement('option');
+        contenidoOptions = document.createTextNode(users[i].name);
+        optionsElements.appendChild(contenidoOptions);
+        selectUsers.appendChild(optionsElements);
+        console.log(users[i].name); 
+        }
+
+    getJSON('../data/cohorts/lim-2018-03-pre-core-pw/progress.json', (err, json) => {
+        if (err) {
+            return console.error(err);
+        }
+        const usersProgress = json;
+
+        console.log(users, usersProgress)
+    });
+});
 
