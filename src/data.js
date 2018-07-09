a8b7140a9f834ec0bd65685bd0aa3c27f441f7d1
natachang <nataliechangch99@gmail.com>
2018-07-09 17:29:30 -0500

    agregando cambios

--- src/data.js
+++ src/data.js
@@ -1,22 +1,89 @@
 window.computeUsersStats = (users, progress, courses) => {
-    //creando objeto stats
-    let stats = {
-        percent: 0,
-        exercises: {
-            total: 0,
-            completed: 0,
-            percent: 0
-        }
-    };
-    //este objeto statss lo voy a meter en mis usuarios, por eso creo una variable
-    let user = users[1];
-    user.stats = stats;
-    return [user, {}, {}]; 
+    console.log('Entré a computeUsersStats!');
+    //Filtrar para validar que todos los usuarios sean estudiantes. 
+    let students = users.filter(user => user.role === 'student');
 
-    users[0].stats = stats;
-    users[1].stats = stats;
-    //stats.percent = 100
-    return [users[1], users[0]]; 
+    //Función para calcular el porcentaje de completitud de cada curso del usuario.
+    const calculatePercent = (user => {
+        let percent = 0;
+        for (let course of courses) {
+            const userProgress = progress[user.id];
+            if (userProgress.hasOwnProperty(course)) {
+                percent += userProgress[course].percent;
+            };
+        };
+        return Math.round(percent / courses.length);
+    });
+
+    //Función para calcular el progreso de cada usuario.
+    const calculateStats = ((user, type) => {
+        let total = 0;
+        let completed = 0;
+        let scoreSum = 0;
+        let scoreAvg = 0;
+        for (let course of courses) {
+            const userProgress = progress[user.id];
+            if (userProgress.hasOwnProperty(course)) {
+                const units = Object.values(userProgress[course].units);
+                for (unit of units) {//recorrido las unidades 
+                    const parts = Object.values(unit.parts);
+                    switch (type) {
+                        case 'practice':
+                            const exercises = parts.filter(part => part.type === 'practice'
+                                && part.hasOwnProperty('exercises'));
+                            for (exercise of exercises) {
+                                total += exercises.length;
+                                completed += exercise.completed;
+                            };
+                            break;
+                        case 'read':
+                            const reads = parts.filter(part => part.type === 'read');
+                            for (read of reads) {
+                                total += reads.length;
+                                completed += read.completed;
+                            };
+                            break;
+                        case 'quiz':
+                            const quizzes = parts.filter(part => part.type === 'quiz'
+                            && part.hasOwnProperty('score'));
+                            for (quiz of quizzes) {
+                                total += quizzes.length;
+                                completed += quiz.completed;
+                                scoreSum += quiz.score;
+                                scoreAvg = Math.round(scoreSum / completed); 
+                            };
+                            break;
+/*                         default:
+                            break; */
+                    }
+                };
+            };
+        };
+        let results = {
+            total: total,
+            completed: completed,
+            percent: Math.round(completed * 100 / total)
+        };
+        if (type === 'quiz') {
+            results.scoreSum = scoreSum;
+            results.scoreAvg = scoreAvg;
+        }
+        return results;
+    });
+
+    //Método map para recorrer array de objetos y obtener un nuevo array
+    students = students.map(user => {
+        const userWithStats = {
+            stats: {
+                percent: calculatePercent(user),
+                exercises: calculateStats(user, 'practice'),
+                reads: calculateStats(user, 'read'),
+                quizzes: calculateStats(user, 'quiz')
+            }
+        };
+        return userWithStats
+    });
+    return students; //array de 727 students
 };
 
 window.sortUsers = (users, orderBy, orderDirection) => {
@@ -27,19 +94,22 @@ window.filterUsers = (users, search) => {
     console.log('Entré a filterUsers!');
 };
 
-//esta funcion retorna un arreglo de usuario con stats ya calculados
+//Función que retorna un arreglo de usuario con stats ya calculados
 window.processCohortData = (options) => {
     console.log('Entré a processCohortData!');
     console.log(options);
-
-    /*     let arrCourses = Object.keys(options.cohort.coursesIndex); */
-    let computedData = computeUsersStats(options.cohortData.users, options.cohortData.progress, [{}]);
+    //Si le hacemos OBJECT KEYS a OPTIONS retorna un array cuyos elementos representan las propiedades del objeto.
+    let courses = Object.keys(options.cohort.coursesIndex);
+    let studentWithStats = computeUsersStats(options.cohortData.users, options.cohortData.progress, courses);
     /*     let sortedData = sortUsers(computedData, options.orderBy, options.orderDirection);
     
         if (options.search !== ' ') {
             let filterData = filterUsers(sortedData, options.search);
             return filterData
-        }
-        return sortedData; */
-    return computedData;
+        } */
+    return studentWithStats; //return sortedData; 
 };
+
+
+
+    
--- src/main.js
+++ src/main.js
@@ -1,109 +1,250 @@
-//Llamando a los elmentos del HTML que se van a manipular
-const selectCohorts = document.getElementById('cohorts');
-const selectUsers = document.getElementById('users');
-const showGlobal = document.getElementById('progress');
-const sedesArr = ['Arequipa', 'Lima', 'México', 'Chile'];
-const buttonMenu = document.getElementById('button-menu');
-const menu = document.getElementById('content-menu');
+
+//Almacenando las variables para la selección de DOM
+const menuButton = document.querySelector('.menu-button')
 const submenu = document.getElementsByClassName('submenu');
-const ulElement1 = document.getElementById('items-sedes');
-const ulElement2 = document.getElementById('items-cohorts');
+const campusesArr = ['aqp', 'cdmx', 'gdl', 'lim', 'scl', 'spl'];
+const listCampus = document.getElementById('items-campuses');
+const listCohort = document.getElementById('items-cohorts');
+const sectionMain = document.querySelector('#main-content');
+const campuses = document.getElementById('campuses');
+const cohorts = document.getElementById('cohorts');
+const optionsList = document.getElementById('options-list');
+const optionsArr = ['perfil', 'configuración', 'cerrar sesión'];
+const userName = document.getElementById('user-name');
+let errorCase = null;
+let campusSection = null;
+let sectionCohort = null;
+let sectionProfile = null;
 
-//Trayendo todos los archivos con su data
-let cohort = []
-let users = []
-let progress = []
 
-const jsonCohort = fetch('../data/cohorts.json')
-  .then(response => response.json())
-const jsonUser = fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
-  .then(response => response.json())
-const jsonUserProgress = fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-  .then(response => response.json())
-Promise.all([jsonCohort, jsonUser, jsonUserProgress])
-  .then(data => {
-    cohort = data[0];
-    users = data[1]
-    progress = data[2]
-    //console.log(data[0], data[1], data[2]);
-  })
-  .catch((err) => {
-    console.error('Error happened' + err);
-  });
-
-//Creando función para mostrar y ocultar el menú contenedor
-const showMenu = () => {
-  if (menu.classList.contains('disabled')) {
-    menu.classList.remove('disabled');
-    menu.classList.add('enabled');
-  } else {
-    menu.classList.remove('enabled');
-    menu.classList.add('disabled');
-  }
-};
-buttonMenu.addEventListener('click', showMenu);
-
-//Creando función para mostrar y ocultar el submenú
-function showSubMenu() {
-  let subMenu = this.getElementsByClassName('items')[0];
-  console.log(subMenu);
-  if (subMenu.classList.contains('hide')) {
-    subMenu.classList.remove('hide');
-    subMenu.classList.add('show');
-  } else {
-    subMenu.classList.remove('show');
-    subMenu.classList.add('hide');
-    console.log(subMenu);
-  }
-}; for (let i = 0; i < submenu.length; i++) {
-  submenu[i].addEventListener('click', showSubMenu);
+//Creando objeto options y definiendo sus valores.
+const options = {
+  cohort: null,
+  cohortData: {
+    users: null,
+    progress: null
+  },
+  orderBy: 'name',
+  orderDirection: 'Ascendente',
+  search: ''
 };
 
-//Creando nodos del DOM: Lista de sedes
-sedesArr.forEach((element, i) => {
-  const listSedes = document.createElement("li");
-  listSedes.textContent = sedesArr[i];
-  listSedes.id = sedesArr[i];
-  ulElement1.appendChild(listSedes);
-  listSedes.addEventListener('click', (e) => {
-    console.log(e.target);
-  })
-});
+//Función para mostrar y ocultar el menú
+const showSidebar = () => {
+  document.body.classList.toggle('sidebar-active');
+};
+menuButton.addEventListener('click', showSidebar);
 
-//Función para traer la data de cohorts de Lima
-const cohortsLima = () => {
-  cohort.forEach((element, i) => {
-    const listCohorts = document.createElement('li');
-    if (cohort[i].id.substring(0, 3) === "lim") {
-      listCohorts.textContent = cohort[i].id;
-      listCohorts.id = cohort[i].id;
-      listCohorts.setAttribute("href", cohort[i].id);
-      ulElement2.appendChild(listCohorts);
-      listCohorts.addEventListener('click', (e) => {
-        console.log(e.target);
-        /*         if (e.target.getAttribute("lim-2018-03-pre-core-pw")) {
-                  console.log("Hola Meli, entraste al cohort de lima 2018"); */
-      })
+//Función para mostrar y ocultar el submenú
+const showCampuses = () => {
+  if (listCampus.style.display === 'block') {
+    listCampus.style.display = 'none';
+  }
+  else {
+    listCampus.style.display = 'block';
+  }
+};
+campuses.addEventListener('click', showCampuses);
+
+const showCohortsList = () => {
+  if (listCohort.style.display === 'block') {
+    listCohort.style.display = 'none';
+  }
+  else {
+    listCohort.style.display = 'block';
+  }
+};
+cohorts.addEventListener('click', showCohortsList);
+
+
+//Función para hacer las peticiones AJAX, usando el método GET
+const getData = (str, url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.addEventListener('load', event => {
+    if (event.target.readyState === 4) {
+      if (event.target.status !== 200) {
+        return console.error(new Error(`HTTP error: ${event.target.status}`));
+      } else {
+        const response = JSON.parse(event.target.responseText);
+        callback(str, response);
+      }
     }
-  });
-}
-selectCohorts.addEventListener('click', cohortsLima);
+  })
+  xhr.send();
+};
 
-showGlobal.addEventListener('click', () => {
-  //Creando objeto options y definiendo sus valores.
-  let options = {
-    cohort: cohort,
-    cohortData: {
-      users: users,
-      progress: progress
-    },
-/*     orderBy: 'name',
-    orderDirection: 'Ascendente',
-    search: '' */
-  };
-  //Llamando a la función processCohortData, y enviando como parametro el objeto options. 
-  //Esto retornará una lista de usuarios con stats, lo cual se almacena en una variable.
+//Función para mostrar la lista de cohorts ya filtrado, según la sede seleccionada 
+const showCohorts = (id, cohortsArr) => {
+  //console.log(id, arrCohorts);
+  const cohortsByCampus = cohortsArr.filter(objCohort => {
+    return objCohort.id.indexOf(id) !== -1;
+  })
+  //console.log(cohortsByCampus);
+  let content = '';
+  cohortsByCampus.forEach((cohort) => {
+    content += `<li id='${cohort.id}' class='listC'>${cohort.id}</li>`
+  })
+  listCohort.innerHTML = content;
+};
+
+//Función para mostrar el progreso de las estudiantes 
+const showProgress = (idCohort, objProgress) => {
+  console.log(idCohort, objProgress); //lim-2018-03-pre-core-pw, objeto de progreso
+  options.cohortData.progress = objProgress;
+  console.log(options);
+  //processCohortData(options)
   let usersWithStats = processCohortData(options);
   console.log(usersWithStats);
+  let progressTable = '';
+  usersWithStats.forEach((user, i) => {
+    let listUser = document.createElement('li');
+    progressTable += `<li> Alumna: ${options.cohortData.users[i].name} <br> 
+      Porcentaje de completitud total: ${user.stats.percent} % <br> 
+      Total de Ejercicios :  ${user.stats.exercises.total} <br>
+      Ejercicios completados: ${user.stats.exercises.completed} <br>
+      Porcentaje de Ejercicios  :  ${user.stats.exercises.percent}  % <br>
+      Total de Lecturas :  ${user.stats.reads.total}<br>
+      Lecturas completadas:  ${user.stats.reads.completed} <br>
+      Porcentaje de Lecturas  : ${user.stats.reads.percent}  % <br>
+      Total de Quizzes :  ${user.stats.quizzes.total} <br>
+      Quizzes completado:  ${user.stats.quizzes.completed} <br>
+      Puntuación promedio en quizzes  :  ${user.stats.quizzes.scoreAvg}</li> <br> `
+    sectionMain.innerHTML = progressTable;
+  })
+};
+
+//Función para mostrar a las estudiantes, según el cohort seleccionado 
+const showStudents = (idCohort, usersArr) => {
+  console.log(idCohort, usersArr);//lim-2018-03-pre-core-pw, arreglo de 735 users, luego creo objeto options
+  //aqui le damos nuevos valores al objeto global options
+  options.cohortData.users = usersArr;
+  console.log(options);
+  getData(idCohort, `../data/cohorts/${idCohort}/progress.json`, showProgress);
+};
+
+//Función para mostrar la data del cohort seleccionado 
+const cohortLima = (idCohort, dataCohorts) => {
+  console.log(idCohort, dataCohorts);//lim , 51 cohorts 
+  dataCohorts.forEach((objCohort) => {
+    //console.log(objCohort); todos los cohorts
+    if (objCohort.id === idCohort) {
+      options.cohort = objCohort;
+    }
+    sectionCohort = '<h1> LIM PRE CORE 2018 PM </h1>'
+    sectionMain.innerHTML = sectionCohort;
+  })
+};
+
+//Creando nodos del DOM: Lista de sedes, con evento CLIC. 
+campusesArr.forEach((element, i) => {
+  const campus = document.createElement('li');
+  campus.textContent = campusesArr[i];
+  campus.id = campusesArr[i];
+  listCampus.appendChild(campus);
 });
+listCampus.addEventListener('click', event => {
+  const id = event.target.id;
+  getData(id, '../data/cohorts.json', showCohorts)
+});
+
+//Eventos del DOM: Lista de cohorts, con evento CLIC. 3
+listCohort.addEventListener('click', event => {
+  const id = event.target.id;
+  console.log(id); //lim-2018-03-pre-core-pw
+  getData(id, '../data/cohorts.json', cohortLima);
+  getData(id, `../data/cohorts/${id}/users.json`, showStudents);
+});
+
+
+
+
+
+
+
+/*
+
+//Función para mostrar la data del cohort seleccionado 
+const cohortSelected = (listCohort) => {
+  if (listCohort === 'lim-2018-03-pre-core-pw') {
+
+  }
+  else {
+    errorCase = '<h3> No hay información sobre este cohort, escoge otro por favor </h3>'
+    sectionMain.innerHTML = errorCase;
+  }
+};
+
+
+//Creando nodos del DOM: Lista de sedes, con evento CLIC. 
+campusesArr.forEach((element, i) => {
+  const campus = document.createElement('li');
+  campus.textContent = campusesArr[i];
+  campus.id = campusesArr[i];
+  listCampus.appendChild(campus);
+});
+
+//Eventos del DOM: Lista de cohorts, con evento CLIC. 
+listCohort.addEventListener('click', event => {
+  const id = event.target.id;
+  getData(id, '../data/cohorts.json', cohortSelected);
+  getData(id, `../data/cohorts/${id}/users.json`, showStudents);
+});
+
+*/ 
+
+const campusSelected = (listCampus) => {
+  if (listCampus === 'lim') {
+    campusSection = '<h1> SEDE LIMA </h1><img class="prom-img" src="images/sede-lima.jpg"><br><img src="images/info-sede-lima.png">'
+    sectionMain.innerHTML = campusSection;
+  }
+  else {
+    errorCase = '<h3> No hay información sobre esta sede, escoge otra por favor </h3>'
+    sectionMain.innerHTML = errorCase;
+  }
+};
+
+
+listCampus.addEventListener('click', event => {
+  const id = event.target.id;
+  getData(id, '../data/cohorts.json', showCohorts)
+  getData(id, '../data/cohorts.json', campusSelected)
+});
+
+
+optionsArr.forEach((element, i) => {
+  const option = document.createElement('li');
+  option.textContent = optionsArr[i];
+  option.id = optionsArr[i];
+  optionsList.appendChild(option);
+});
+
+
+const showOptionsList = () => {
+  if (optionsList.style.display === 'block') {
+    optionsList.style.display = 'none';
+  }
+  else {
+    optionsList.style.display = 'block';
+  }
+};
+
+userName.addEventListener('click', showOptionsList);
+
+const optionSelected = (optionsList) => {
+
+  if (optionsList === 'perfil') {
+    sectionProfile = ' <img src="images/ale.png"> <h1> Alejandra Ramirez </h1>'
+    sectionMain.innerHTML = sectionProfile;
+  }
+  else if (optionsList === 'cerrar sesión') {
+    window.location.assign("index.html")
+  }
+};
+
+optionsList.addEventListener('click', event => {
+  const id = event.target.id;
+  getData(id, '../data/cohorts.json', optionSelected);
+});
+ 
 
