3fb91349f96dccc41fc9052348fcf5b1ffc667a0
marimeli <marimel.casas@gmail.com>
2018-07-11 21:28:26 -0500

    agregando funciones

---
+++ src/app..js
@@ -0,0 +1,13 @@
+/* Lizeth
+Percent: 53 %
+    Total de Ejercicios: 2
+Total de Ejercicios de completo: 0
+Porcentaje de Exercises: 0 %
+    Total de Lecturas: 11
+Total de Lecturas que completo: 6
+Porcentaje de Lecturas: 55 %
+    Total de Quizzes: 3
+Total de Quizzes que completo: 2
+Porcentaje de Quizzes: 67 %
+ 
+ */
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ const cohorts = document.getElementById('cohorts');
 const optionsList = document.getElementById('options-list');
 const optionsArr = ['perfil', 'configuración', 'cerrar sesión'];
 const userName = document.getElementById('user-name');
+const selectOrder = document.getElementById('select-order');
 let errorCase = null;
 let campusSection = null;
 let sectionCohort = null;
@@ -69,21 +70,31 @@ const showCohorts = (id, cohortsArr) => {
 
 //Función para mostrar el progreso de las estudiantes 8
 const showProgress = (idCohort, objProgress) => {
-  console.log(idCohort, objProgress); //lim-2018-03-pre-core-pw, objeto de progreso
   options.cohortData.progress = objProgress;
-  console.log(options);
-  //processCohortData(options)
- let usersWithStats = processCohortData(options);
-  console.log(usersWithStats); 
-   let progressTable = '';
+  let usersWithStats = processCohortData(options);
+  let progressTable = '';
+  progressTable = `<input id="search" class="search" type="text" placeholder="Buscar">
+  <select id="select-order" class="selector" placeholder="Ordenar por">
+  <option value="ordenar-por">ORDENAR POR</option>
+  <option value="name">Name</option>
+  <option value="total-completed">TotalCompleted</option>
+  <option value="exercises-percent">ExercisesPercent</option>
+  <option value="quizzes-percent">QuizzesPercent</option>
+  <option value="quizzes-average-score">QuizzesAverageScore</option>
+  <option value="reads-percent">ReadsPercent</option> 
+ </select> 
+ <button id="buttonOrder" class="button-order">Ascendente</button> <br> <br>`
+ 
+  progressTable +=  `<br> <br> <h1> LIM PRE CORE 2018 PM </h1> <br> <br>`
+  sectionMain.innerHTML = progressTable;
   usersWithStats.forEach((user, i) => {
     let listUser = document.createElement('li');
-    progressTable += `<li> Alumna: ${options.cohortData.users[i].name} <br> 
-      Porcentaje de completitud total: ${user.stats.percent} % <br> 
+    progressTable += `<li> Alumna: ${options.cohortData.users[i].name} <br>
+      Porcentaje de completitud total: ${user.stats.percent} % <br>
       Total de Ejercicios :  ${user.stats.exercises.total} <br>
       Ejercicios completados: ${user.stats.exercises.completed} <br>
       Porcentaje de Ejercicios  :  ${user.stats.exercises.percent}  % <br>
-      Total de Lecturas :  ${user.stats.reads.total }<br>
+      Total de Lecturas :  ${user.stats.reads.total}<br>
       Lecturas completadas:  ${user.stats.reads.completed} <br>
       Porcentaje de Lecturas  : ${user.stats.reads.percent}  % <br>
       Total de Quizzes :  ${user.stats.quizzes.total} <br>
@@ -91,46 +102,48 @@ const showProgress = (idCohort, objProgress) => {
       Puntuación promedio en quizzes  :  ${user.stats.quizzes.scoreAvg}</li> <br> `
     sectionMain.innerHTML = progressTable;
   }) 
-};
+ };
+ 
+  //Función para mostrar a las estudiantes, según el cohort seleccionado 7
+  const showStudents = (idCohort, usersArr) => {
+    //console.log(idCohort, usersArr);//lim-2018-03-pre-core-pw, arreglo de 735 users, luego creo objeto options
+    //aqui le damos nuevos valores al objeto global options
+    options.cohortData.users = usersArr;
+    //console.log(options);
+    getData(idCohort, `../data/cohorts/${idCohort}/progress.json`, showProgress);
+  };
 
-//Función para mostrar a las estudiantes, según el cohort seleccionado 7
-const showStudents = (idCohort, usersArr) => {
-  console.log(idCohort, usersArr);//lim-2018-03-pre-core-pw, arreglo de 735 users, luego creo objeto options
-  //aqui le damos nuevos valores al objeto global options
-  options.cohortData.users = usersArr;
-  console.log(options);
-  getData(idCohort, `../data/cohorts/${idCohort}/progress.json`, showProgress);
-};
+  //Función para mostrar la data del cohort seleccionado 6
+  const cohortLima = (idCohort, dataCohorts) => {
+    //console.log(idCohort, dataCohorts);//lim , 51 cohorts 
+    dataCohorts.forEach((objCohort) => {
+      //console.log(objCohort); todos los cohorts
+      if (objCohort.id === idCohort) {
+        options.cohort = objCohort;
+      }
+      sectionCohort = '<h1> LIM PRE CORE 2018 PM </h1>'
+      sectionMain.innerHTML = sectionCohort;
+    })
+  };
 
-//Función para mostrar la data del cohort seleccionado 6
-const cohortLima = (idCohort, dataCohorts) => {
-  console.log(idCohort, dataCohorts);//lim , 51 cohorts 
-  dataCohorts.forEach((objCohort) => {
-    //console.log(objCohort); todos los cohorts
-    if (objCohort.id === idCohort) {
-      options.cohort = objCohort;
-    }
-    sectionCohort = '<h1> LIM PRE CORE 2018 PM </h1>'
-    sectionMain.innerHTML = sectionCohort;
-  })
-};
+  //Creando nodos del DOM: Lista de sedes, con evento CLIC. 1
+  campusesArr.forEach((element, i) => {
+    const campus = document.createElement('li');
+    campus.textContent = campusesArr[i];
+    campus.id = campusesArr[i];
+    listCampus.appendChild(campus);
+  });
+  listCampus.addEventListener('click', event => {
+    const id = event.target.id;
+    getData(id, '../data/cohorts.json', showCohorts)
+  });
 
-//Creando nodos del DOM: Lista de sedes, con evento CLIC. 1
-campusesArr.forEach((element, i) => {
-  const campus = document.createElement('li');
-  campus.textContent = campusesArr[i];
-  campus.id = campusesArr[i];
-  listCampus.appendChild(campus);
-});
-listCampus.addEventListener('click', event => {
-  const id = event.target.id;
-  getData(id, '../data/cohorts.json', showCohorts)
-});
+  //Eventos del DOM: Lista de cohorts, con evento CLIC. 3
+  listCohort.addEventListener('click', event => {
+    const id = event.target.id;
+    //console.log(id); //lim-2018-03-pre-core-pw
+    getData(id, '../data/cohorts.json', cohortLima);
+    getData(id, `../data/cohorts/${id}/users.json`, showStudents);
+  });
 
-//Eventos del DOM: Lista de cohorts, con evento CLIC. 3
-listCohort.addEventListener('click', event => {
-  const id = event.target.id;
-  console.log(id); //lim-2018-03-pre-core-pw
-  getData(id, '../data/cohorts.json', cohortLima);
-  getData(id, `../data/cohorts/${id}/users.json`, showStudents);
-});
+
